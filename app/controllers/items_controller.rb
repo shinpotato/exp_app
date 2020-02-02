09b4ba680d03283be82750f6c38fdaca954b0f91
shinpotato <potatoandtomato2@gmail.com>
2020-02-02 10:46:52 +0900

    add_edit_function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,10 +13,20 @@ class ItemsController < ApplicationController
     Item.create(language: item_params[:language], keyword: item_params[:keyword], exp_category: item_params[:exp_category], exp_detail: item_params[:exp_detail], user_id: current_user.id)
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
 
+#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝以下プラベート＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
   private
 
   def item_params
