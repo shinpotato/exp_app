e2f1c0c972646289ccfa786af39a08f3cc3a94c0
shinpotato <potatoandtomato2@gmail.com>
2020-02-02 12:06:32 +0900

    add_validation_to_items and_pulldown

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :language, {presence: true}
+  validates :keyword, {presence: true}
+  validates :exp_category, {presence: true}
+  validates :exp_detail, {presence: true}
 end
