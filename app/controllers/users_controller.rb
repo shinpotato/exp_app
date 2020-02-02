595c59568054a2239807a7cd9d92d7fe29594670
shinpotato <potatoandtomato2@gmail.com>
2020-02-02 15:33:38 +0900

    improve_layout_and_add_search_window

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
   def show
 #    @items = Item.where(user_id: current_user.id)
     @user = User.find(current_user.id)
+    #スマートではないが動く
+    @lang_hash = {
+                  "git" => "Git", "ruby" => "Ruby", "rails" => "Rails",
+                  "aws" => "AWS", "php" => "PHP",
+                  "info" => "情報発信", "writing" => "ライティング講座",
+    }
+    @exp_categ_hash = {
+                  "finding" => "気付き",
+                  "question" => "疑問",
+                  "solution" => "解決",
+    }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Item.create!(language: "git", keyword: "merge", exp_category: "気付き", exp_detail: "マージする際は元と対象を確認する必要がある", user_id: 1)
-Item.create!(language: "ruby", keyword: "seeds.rb", exp_category: "疑問", exp_detail: "seeds.rbは何の役割がある？", user_id: 2)
-Item.create!(language: "rails", keyword: "bundle", exp_category: "解決", exp_detail: "bundlerのバージョンをlockファイルと合わせたらエラーが消えた", user_id: 3)
+Item.create!(language: "git", keyword: "merge", exp_category: "finding", exp_detail: "マージする際は元と対象を確認する必要がある", user_id: 1)
+Item.create!(language: "ruby", keyword: "seeds.rb", exp_category: "question", exp_detail: "seeds.rbは何の役割がある？", user_id: 2)
+Item.create!(language: "rails", keyword: "bundle", exp_category: "solution", exp_detail: "bundlerのバージョンをlockファイルと合わせたらエラーが消えた", user_id: 3)
